8b32dcf1d2e01638aa4aa0de677f4801d1046305
Rodney Foley <@CreepyGnome>
2012-12-17 14:00:41 -0700

    Revert "Moved GetType call out of if blocks"
    This reverts commit 3fd1d9cb2d400509840925099e6879c87af7e8c8.

--- Massive.cs
+++ Massive.cs
@@ -68,13 +68,12 @@ namespace Massive {
         public static dynamic ToExpando(this object o) {
             var result = new ExpandoObject();
             var d = result as IDictionary<string, object>; //work with the Expando as a Dictionary
-            Type oType = o.GetType();
-            if (oType == typeof(ExpandoObject)) return o; //shouldn't have to... but just in case
-            if (oType == typeof(NameValueCollection) || oType.IsSubclassOf(typeof(NameValueCollection))) {
+            if (o.GetType() == typeof(ExpandoObject)) return o; //shouldn't have to... but just in case
+            if (o.GetType() == typeof(NameValueCollection) || o.GetType().IsSubclassOf(typeof(NameValueCollection))) {
                 var nv = (NameValueCollection)o;
-                nv.Cast<string>().Select(key => new KeyValuePair<string, object>(key, nv[key])).ToList().ForEach(d.Add);
+                nv.Cast<string>().Select(key => new KeyValuePair<string, object>(key, nv[key])).ToList().ForEach(i => d.Add(i));
             } else {
-                var props = oType.GetProperties();
+                var props = o.GetType().GetProperties();
                 foreach (var item in props) {
                     d.Add(item.Name, item.GetValue(o, null));
                 }
