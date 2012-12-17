3fd1d9cb2d400509840925099e6879c87af7e8c8
Rodney Foley <@CreepyGnome>
2012-12-17 13:31:04 -0700

    Moved GetType call out of if blocks
    Moved GetType call out of the if blocks which was calling it up to 4
    times, now it is only called once always.

--- Massive.cs
+++ Massive.cs
@@ -68,12 +68,13 @@ namespace Massive {
         public static dynamic ToExpando(this object o) {
             var result = new ExpandoObject();
             var d = result as IDictionary<string, object>; //work with the Expando as a Dictionary
-            if (o.GetType() == typeof(ExpandoObject)) return o; //shouldn't have to... but just in case
-            if (o.GetType() == typeof(NameValueCollection) || o.GetType().IsSubclassOf(typeof(NameValueCollection))) {
+            Type oType = o.GetType();
+            if (oType == typeof(ExpandoObject)) return o; //shouldn't have to... but just in case
+            if (oType == typeof(NameValueCollection) || oType.IsSubclassOf(typeof(NameValueCollection))) {
                 var nv = (NameValueCollection)o;
-                nv.Cast<string>().Select(key => new KeyValuePair<string, object>(key, nv[key])).ToList().ForEach(i => d.Add(i));
+                nv.Cast<string>().Select(key => new KeyValuePair<string, object>(key, nv[key])).ToList().ForEach(d.Add);
             } else {
-                var props = o.GetType().GetProperties();
+                var props = oType.GetProperties();
                 foreach (var item in props) {
                     d.Add(item.Name, item.GetValue(o, null));
                 }
