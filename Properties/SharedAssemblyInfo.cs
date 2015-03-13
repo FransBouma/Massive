532b6e54cb97e89758aebf443b260d6d85b869b5
Frans Bouma <frans@sd.nl>
2015-03-13 18:07:53 +0100

    Forgot 2 files, updated gitignore.

---
+++ Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Massive")]
+[assembly: AssemblyConfiguration(".NET 4.0+")]
+[assembly: AssemblyProduct("Massive")]
+[assembly: AssemblyCopyright("Copyright © 2009-2015 various contributors")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// version
+[assembly: AssemblyVersion("1.1.0.0")]
+// version + builddate (yy.mmdd)
+[assembly: AssemblyFileVersion("1.1.15.0313")]
